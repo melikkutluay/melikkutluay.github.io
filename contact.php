5e62390d0b95f83d5b2f117bd3f5e1305e6cb840
melikkutluay <milliler04@gmail.com>
2017-04-10 15:20:12 +0300

    okey

---
+++ contact.php
@@ -0,0 +1,6 @@
+<?php
+$from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+		      echo "Email sent!";
+          echo "oldu ise yazdırılır";
+ ?>
