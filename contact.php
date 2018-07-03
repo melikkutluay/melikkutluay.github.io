ac4efec655fde739bdc37f5908ba4c8c9c58ad82
melikkutluay <milliler04@gmail.com>
2018-07-03 17:42:54 +0300

    Delete contact.php

--- contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-$from="From: $name<$email>\r\nReturn-path: $email";
-        $subject="Message sent using your contact form";
-		      echo "Email sent!";
-          echo "oldu ise yazdırılır";
- ?>
